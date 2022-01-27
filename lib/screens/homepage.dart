b0870eda467d376233aef9e1b56b7bb5d50752d1
Javier <javierneri81@gmail.com>
2022-01-27 18:50:01 +0800

    Added scaffold to homepage with an app bar title of 'Coffee Journal'.

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -5,6 +5,11 @@ class HomepageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Coffee Journal'),
+        centerTitle: true,
+      ),
+    );
   }
 }
