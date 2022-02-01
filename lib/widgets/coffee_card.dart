420be5db86ba1c10a8c63c7adbc0d8da4e0fb7fc
Javier <javierneri81@gmail.com>
2022-02-02 01:03:45 +0800

    created coffee card widget for homepage

---
+++ lib/widgets/coffee_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CoffeeCard extends StatelessWidget {
+  const CoffeeCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
