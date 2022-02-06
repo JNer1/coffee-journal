3f43b87ffcebdbe7e9ed20a6be82689485c0b968
Javier <javierneri81@gmail.com>
2022-02-06 15:25:57 +0800

    changed coffee_card to stateful widget and extracted CoffeeName to it's own widget

--- lib/widgets/coffee_card.dart
+++ lib/widgets/coffee_card.dart
@@ -1,14 +1,35 @@
+import 'package:coffee_journal/chip_data.dart';
 import 'package:flutter/material.dart';
 
-class CoffeeCard extends StatelessWidget {
+class CoffeeCard extends StatefulWidget {
   const CoffeeCard({Key? key}) : super(key: key);
+
+  @override
+  State<CoffeeCard> createState() => _CoffeeCardState();
+}
+
+class _CoffeeCardState extends State<CoffeeCard> {
+  final List<TasteChipData> _allTasteChips = [];
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Column(
-        children: [],
+        children: const [
+          CoffeeName(),
+        ],
       ),
     );
+  }
+}
+
+class CoffeeName extends StatelessWidget {
+  const CoffeeName({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Coffee');
   }
 }
