e1d854628b9e65296886ae025a19e5c6259b441f
Javier <javierneri81@gmail.com>
2022-02-07 15:54:43 +0800

    made coffee list into separate stateful widget

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:coffee_journal/widgets/coffee_card.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/coffee_list.dart';
 
 class HomepageScreen extends StatefulWidget {
   const HomepageScreen({Key? key}) : super(key: key);
@@ -10,9 +10,6 @@ class HomepageScreen extends StatefulWidget {
 }
 
 class _HomepageScreenState extends State<HomepageScreen> {
-  final Stream _coffeeCardStream =
-      FirebaseFirestore.instance.collection('coffee').snapshots();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,51 +17,7 @@ class _HomepageScreenState extends State<HomepageScreen> {
         title: const Text('Coffee Journal'),
         centerTitle: true,
       ),
-      body: StreamBuilder(
-        stream: _coffeeCardStream,
-        builder: (context, AsyncSnapshot snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasError) {
-            showErrorMessage();
-          }
-          return CustomScrollView(
-            slivers: [
-              SliverPadding(
-                padding: const EdgeInsets.all(8),
-                sliver: SliverList(
-                    delegate: SliverChildBuilderDelegate((context, index) {
-                  return CoffeeCard(
-                    coffeeName: getName(snapshot, index),
-                    tasteNotes: getNotes(snapshot, index),
-                  );
-                }, childCount: snapshot.data.docs.length)),
-              )
-            ],
-          );
-        },
-      ),
-    );
-  }
-
-  getNotes(AsyncSnapshot<dynamic> snapshot, int index) =>
-      snapshot.data.docs[index].get('tasteNotes');
-
-  getName(AsyncSnapshot<dynamic> snapshot, int index) =>
-      snapshot.data.docs[index].get('name');
-
-  Column showErrorMessage() {
-    return Column(
-      children: const [
-        Icon(
-          Icons.error,
-          color: Colors.orange,
-        ),
-        Text('Error')
-      ],
+      body: const CoffeeList(),
     );
   }
 }
