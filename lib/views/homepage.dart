246a463f21372b9e8e060ba2f28fe09a9533cf6c
Javier <javierneri81@gmail.com>
2022-02-08 03:27:49 +0800

    added temporary logout button

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/coffee_list.dart';
@@ -14,10 +15,30 @@ class _HomepageScreenState extends State<HomepageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                    title: const Text('Log out?'),
+                    actions: [
+                      TextButton(onPressed: logOut, child: const Text('Yes'))
+                    ],
+                  ),
+                );
+              },
+              icon: const Icon(Icons.arrow_upward))
+        ],
         title: const Text('Coffee Journal'),
         centerTitle: true,
       ),
       body: const CoffeeList(),
     );
+  }
+
+  void logOut() {
+    FirebaseAuth.instance.signOut();
+    Navigator.of(context).pop();
   }
 }
